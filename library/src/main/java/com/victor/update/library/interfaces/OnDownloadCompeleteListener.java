cd6ed8f31bdab3b945e775dfad1abd9a4940a41e
Victor2018 <zhanghaifeng903@sina.com>
2016-12-15 17:44:48 +0800

    first_commit

---
+++ library/src/main/java/com/victor/update/library/interfaces/OnDownloadCompeleteListener.java
@@ -0,0 +1,8 @@
+package com.victor.update.library.interfaces;
+
+/**
+ * Created by victor on 2016/9/2.
+ */
+public interface OnDownloadCompeleteListener {
+    void onDownloadComplete();
+}
---
+++ library/src/main/java/com/victor/update/library/interfaces/OnUpdateCompleteListener.java
@@ -0,0 +1,10 @@
+package com.victor.update.library.interfaces;
+
+import android.os.Bundle;
+
+/**
+ * Created by victor on 2016/9/2.
+ */
+public interface OnUpdateCompleteListener {
+    void onUpdateCompelete(Bundle result);
+}
